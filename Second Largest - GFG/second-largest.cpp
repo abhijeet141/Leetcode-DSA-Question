8be68c1fe93bcac67aacc14231b4b6b540665243
Abhijeet Sinha <67696867+abhijeet141@users.noreply.github.com>
2023-03-28 15:03:53 +0530

    Added solution - LeetHub

--- Second Largest - GFG/second-largest.cpp
+++ Second Largest - GFG/second-largest.cpp
@@ -10,21 +10,31 @@ public:
 	// Function returns the second
 	// largest elements
 	int print2largest(int a[], int n) {
-	int res = -1, largest = 0;
-	for(int i = 0;i<n;i++){
-	    if(a[i]>largest){
-	        largest = a[i];
-	    }
-	}
-	for(int i=0;i<n;i++){
-	    if(a[i] == largest){
-	        continue;
-	    }
-	    else if(a[i]>res){
-	        res = a[i];
-	    }
-	}
-	return res;
+	int res = -1, largest = -1;
+// 	for(int i = 0;i<n;i++){
+// 	    if(a[i]>largest){
+// 	        largest = a[i];
+// 	    }
+// 	}
+// 	for(int i=0;i<n;i++){
+// 	    if(a[i] == largest){
+// 	        continue;
+// 	    }
+// 	    else if(a[i]>res){
+// 	        res = a[i];
+// 	    }
+// 	}
+// 	return res;
+for(int i = 0;i<n;i++){
+    if(a[i]>largest){
+        res = largest;
+        largest = a[i];
+    }
+    else if(a[i]!=largest and res<a[i]){
+        res = a[i];
+    }
+}
+return res;
 	}
 };
 
